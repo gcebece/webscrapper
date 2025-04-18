d7ffea398f762123b5a8de97988cdd075718fff5
gcebece <kumaravelprogrammer@gmail.com>
2025-04-18 21:40:44 +0530

    Update configuration for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/webscrapper' : '',
+  images: {
+    unoptimized: true,
+  },
   async headers() {
     return [
       {
